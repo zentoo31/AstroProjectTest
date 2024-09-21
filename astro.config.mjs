8cd2ea1613b0759def82af802deabe03dbcf6015
zentoo31 <zentoo31@gmail.com>
2024-09-21 18:36:21 -0500

    21/09/24

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  integrations: [tailwind()],
+  env:{
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean( {default: true ,context: "server", access: "public"}),
+      SCORE_API_ENDPOINT: envField.string({ context: "server", access: "public"})
+    }
+  }
 });
